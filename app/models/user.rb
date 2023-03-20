cd52f2f5ac9a9e728fc13793dc9e8de4f5448441
mkaya13 <53873573+mkaya13@users.noreply.github.com>
2023-03-20 21:21:02 +0300

    Adds validations to the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_many :groups, class_name: 'Group'
   has_many :payments, class_name: 'Payment'
+
+  validates :name, presence: true
+  validates :name, length: { minimum: 2, maximum: 50 }
+
 end

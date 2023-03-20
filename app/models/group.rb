aa98137df035f8000bceea909568ba906d479456
mkaya13 <53873573+mkaya13@users.noreply.github.com>
2023-03-20 21:25:19 +0300

    Fixes linter errors

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :payments
-  
+
 
   validates :name, presence: true
   validates :icon, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :name, length: { minimum: 2, maximum: 50 }
-
 end

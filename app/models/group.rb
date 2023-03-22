ff2f25b80fecffee5a5753a50403f7b62c44b06d
mkaya13 <53873573+mkaya13@users.noreply.github.com>
2023-03-22 12:44:04 +0300

    Desktop views and implement destroy groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
   belongs_to :user
-  has_and_belongs_to_many :payments
+  has_and_belongs_to_many :payments, dependent: :destroy
 
   validates :name, presence: true
   validates :icon, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-
   root "groups#index"
 
-
   resources :users, only: [:index, :show] do
-    resources :groups, only: [:new, :create, :index, :show] do
+    resources :groups, only: [:new, :create, :index, :show, :destroy] do
       resources :payments, only: [:new, :create, :index, :show] do
       end
     end
@@ -18,8 +11,4 @@ Rails.application.routes.draw do
     resources :payments, only: [:index, :new, :create] do
     end
   end
-
-  # resources :payments, only: [:new, :create, :index] do
-  # end
-
 end
